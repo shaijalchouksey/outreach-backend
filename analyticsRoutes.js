84653fc3233d88c0b6dc95cd6fe4d9671dfa485c
shaijal chouksey <154827347+shaijalchouksey@users.noreply.github.com>
2025-11-12 22:41:23 +0530

    Update and rename analyticsRoutes.js to w

--- analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logSearch } = require('../controllers/analyticsController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-
-router.post('/log-search', authMiddleware, logSearch);
-
-module.exports = router;
